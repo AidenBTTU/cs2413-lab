a7b0c1c3bbf73eb49c07c48f89974f20a1585f76
AidenBTTU <aidenbuch06@gmail.com>
2026-01-21 12:35:14 -0600

    Finish lab00

--- lab00/src/main.c
+++ lab00/src/main.c
@@ -4,9 +4,9 @@ int main(void) {
     printf("CS2413 Lab00 Starter Demo\n");
 
     // TODO: Replace the placeholder information below
-    printf("Name: YOUR_NAME_HERE\n");
+    printf("Name: Aiden Buchanan\n");
     printf("R#: ****XXXXX\n");
-    printf("Section: XXX\n");
+    printf("Section: 506\n");
     printf("Message: I can use GitHub for CS2413 labs!\n");
 
     return 0;
